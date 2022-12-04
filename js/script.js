19255c011815a14eb9d4824d2dee8e33d6476595
micheleTummolo <tummolomichele02@gmail.com>
2022-12-04 19:58:36 +0100

    Done style

--- js/script.js
+++ js/script.js
@@ -5,7 +5,10 @@ let item;
  
 while (answer == "Y") {
     item = prompt("Inserisci articolo")
-    li.innerHTML += `<li> ${item} </li>`;
+    li.innerHTML += `<div class="d-flex align-items-center">
+                        <input type="checkbox">
+                        <label class="items fw-semibold ms-1 mb-1"> ${item} </label><br>
+                    </div>`;
     console.log(li);
     answer = prompt("Vuoi inserire un altro articolo? (Y/N)")
     answer = answer.toUpperCase()
